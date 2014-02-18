1a1f6f05dba3bd4123313716e4714c0d1358437d
Shiyanov Sergey <shiyanov@imhonet.ru>
2014-02-18 16:15:49 +0400

    data formaters for memcached set and get responses

---
+++ tests/DataFormat/Bool/Memcached/SetTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Imhonet\Connection\Test\DataFormat\Bool\Memcached;
+
+use Imhonet\Connection\DataFormat\Bool\Memcached\Set;
+use Imhonet\Connection\DataFormat\IBool;
+
+class SetTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var IBool
+     */
+    private $formater;
+
+    protected function setUp()
+    {
+        $this->formater = new Set();
+    }
+
+    public function testCreate()
+    {
+        $this->assertInstanceOf('Imhonet\Connection\DataFormat\IBool', $this->formater);
+    }
+
+    public function testData()
+    {
+        $this->formater->setData(true);
+        $this->assertEquals($this->formater->formatData(), array());
+    }
+
+    public function testValue()
+    {
+        $this->formater->setData(true);
+        $this->assertTrue($this->formater->formatValue());
+    }
+
+    public function testFailure()
+    {
+        $this->formater->setData(false);
+        $this->assertFalse($this->formater->formatValue());
+    }
+
+    protected function tearDown()
+    {
+        $this->formater = null;
+    }
+
+}
