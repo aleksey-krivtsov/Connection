0fbe18e0613a9814d67b6755d0126b29d0622050
Shiyanov Sergey <shiyanov@imhonet.ru>
2014-02-20 17:37:35 +0400

    add memcache get and set queries

---
+++ src/Query/Memcache/Set.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Imhonet\Connection\Query\Memcache;
+
+use Imhonet\Connection\Query\Base;
+use Respect\Validation\Validator;
+
+class Set extends Base
+{
+    private $data = array();
+    private $expire = 0;
+    private $count = 0;
+
+    /**
+     * @var bool
+     */
+    private $response;
+
+    /**
+     * @param array $data [key => value, ...]
+     */
+    public function setData(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function setExpire($value)
+    {
+        assert(Validator::create()->digit()->noWhitespace()->validate($value));
+
+        $this->expire = $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute()
+    {
+        return $this->getResponse();
+    }
+
+    private function getResponse()
+    {
+        if ($this->response === null) {
+            $this->response = true;
+            $resource = $this->getResource();
+
+            foreach ($this->data as $key => $value) {
+                if ($resource->set($key, $value, 0, $this->expire)) {
+                    ++$this->count;
+                } else {
+                    $this->response = false;
+                }
+            }
+        }
+
+        return $this->response;
+    }
+
+    /**
+     * @return \Memcache
+     */
+    private function getResource()
+    {
+        return $this->resource->getHandle();
+    }
+
+    private function isError()
+    {
+        return $this->getResponse() === false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getErrorCode()
+    {
+        return (int) $this->isError();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCountTotal()
+    {
+        return $this->getCount();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCount()
+    {
+        $this->getResponse();
+        return $this->count;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getLastId()
+    {
+    }
+
+}
---
+++ tests/Query/Memcache/GetTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Imhonet\Connection\Test\Query\Memcache;
+
+use Imhonet\Connection\Query\Memcache\Get;
+use Imhonet\Connection\Resource\IResource;
+
+class GetTest extends \PHPUnit_Framework_TestCase
+{
+    private $data = array(
+        'false' => false,
+        'true' => true,
+        'null' => null,
+        'int' => 42,
+        'string' => 'forty two',
+        'arr' => array(42),
+    );
+
+    /**
+     * @var Get
+     */
+    private $query;
+
+    protected function setUp()
+    {
+        $this->query = new Get();
+        $this->query->setKeys(array_keys($this->data));
+    }
+
+    public function testCreate()
+    {
+        $this->assertInstanceOf('Imhonet\Connection\Query\Base', $this->query);
+    }
+
+    public function testExecute()
+    {
+        $this->query->setResource($this->getResource());
+        $this->assertEquals($this->data, $this->query->execute());
+    }
+
+    public function testCount()
+    {
+        $this->query->setResource($this->getResource());
+        $this->assertEquals(sizeof($this->data), $this->query->getCount());
+        $this->assertEquals(sizeof($this->data), $this->query->getCountTotal());
+    }
+
+    public function testErrorCode()
+    {
+        $this->query->setResource($this->getResource());
+        $this->assertEquals(0, $this->query->getErrorCode());
+    }
+
+    public function testFailure()
+    {
+        $this->query->setResource($this->getResourceFailed());
+        $this->assertEquals(false, $this->query->execute());
+    }
+
+    public function testFailureCount()
+    {
+        $this->query->setResource($this->getResourceFailed());
+        $this->assertEquals(0, $this->query->getCount());
+        $this->assertEquals(0, $this->query->getCountTotal());
+    }
+
+    public function testFailureErrorCode()
+    {
+        $this->query->setResource($this->getResourceFailed());
+        $this->assertNotEquals(0, $this->query->getErrorCode());
+    }
+
+    /**
+     * @return IResource
+     */
+    private function getResource()
+    {
+        $memcache = $this->getMock('\Memcache', array('get'));
+        $memcache
+            ->expects($this->any())
+            ->method('get')
+            ->with(array_keys($this->data))
+            ->will($this->returnValue($this->data))
+        ;
+
+        $resource = $this->getMock('Imhonet\Connection\Resource\Memcache', array('getHandle'));
+        $resource
+            ->expects($this->any())
+            ->method('getHandle')
+            ->will($this->returnValue($memcache))
+        ;
+
+        return $resource;
+    }
+
+    /**
+     * @return IResource
+     */
+    private function getResourceFailed()
+    {
+        $memcache = $this->getMock('\Memcache', array('get'));
+        $memcache
+            ->expects($this->any())
+            ->method('get')
+            ->withAnyParameters()
+            ->will($this->returnValue(false))
+        ;
+
+        $resource = $this->getMock('Imhonet\Connection\Resource\Memcache', array('getHandle'));
+        $resource
+            ->expects($this->any())
+            ->method('getHandle')
+            ->will($this->returnValue($memcache))
+        ;
+
+        return $resource;
+    }
+
+    protected function tearDown()
+    {
+        $this->query = null;
+    }
+
+}
